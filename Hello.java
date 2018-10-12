1411d800ea4bd6fca5516cc5d1537a73874b9dc9
Madhav Gopal <madhav.gopal@synapse.com>
2018-10-12 15:59:06 +0530

    new code added

--- Hello.java
+++ Hello.java
@@ -4,5 +4,11 @@ public class Hello{
 		System.out.print("Hello!");
 		System.out.print("1");
 		System.out.print("2");
+
+		int i=0;
+		for(int j=0;j<2;j++){
+			System.out.print("i");
+			i=i+2;
 			}
+		}
 }
