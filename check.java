cd338a1e4c764532ca0fec5ab3b979f415a4a7a7
Madhav Gopal <madhav.gopal@synapse.com>
2018-10-12 16:54:03 +0530

    for loop added in check.java

--- check.java
+++ check.java
@@ -3,5 +3,9 @@ public class check{
 		System.out.print("check!");
 		System.out.print("check 0");
 		System.out.print("check 1");
+
+		for(int i=1;i<=4;i++){
+			System.out.println("yes"+i);
+		}
 	}
 }
