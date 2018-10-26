ec0b0628f703f376c18c944db135cc2502ebadc0
madhav.gopal <madhavg306@gmail.com>
2018-10-26 12:21:13 +0530

    Changes made A print statement was deleted

--- check.java
+++ check.java
@@ -1,8 +1,7 @@
 public class check{
 	public static void main(String[] args) {
 		System.out.print("check!");
-		System.out.print("check 0");
-		System.out.print("check 1");
+		System.out.println("check 0");
 
 		for(int i=1;i<=4;i++){
 			System.out.println("yes"+i);
